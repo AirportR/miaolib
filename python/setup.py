511762f581a9ef270dd428515be39c1d5ea8e5f8
koipy-org <aasg9414@gmail.com>
2025-02-07 14:39:05 +0800

    :memo: v0.1.2

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="miaospeedlib",
-    version="0.1.1",
+    version="0.1.2",
     author="airportr",
     url='https://github.com/AirportR/miaolib',
     author_email="airportroster@gmail.com",
